8b08bdbd055be3c22a075386adc1d92ca8506dfe
amitzyuwdcvala <amitvala065@gmail.com>
2026-08-04 08:20:20 +0530

    Update subscription expiration check frequency to every six hours

--- routes/console.php
+++ routes/console.php
@@ -9,6 +9,6 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('subscriptions:check-expiration')
-    ->hourly()
+    ->everySixHours()
     ->timezone('Asia/Kolkata')
     ->withoutOverlapping();
